8e9cfb3aee7a95776c95d9bb4b53ce9b4906bba4
Jai@Khare <113430371+senseiJai@users.noreply.github.com>
2024-09-23 13:28:29 +0530

    Time: 25 ms (22.97%), Space: 28.3 MB (24.86%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k=k%nums.size();
-        int s=0; 
-        int e=nums.size()-1;
-        while(s<e){
-            swap(nums[s++],nums[e--]);
+        // k=k%nums.size();
+        // int s=0; 
+        // int e=nums.size()-1;
+        // while(s<e){
+        //     swap(nums[s++],nums[e--]);
+        // }
+        // int st=k;
+        // int en=nums.size()-1;
+        // while(st<en){
+        //     swap(nums[st++],nums[en--]);
+        // }
+        // int start=0;
+        // int end=k-1;
+        // while(start<end){
+        //     swap(nums[start++],nums[end--]);
+        // }
+        int n= nums.size();
+        k = k%n;
+        vector<int> rotated(n);
+        for(int i= 0; i<n; i++){
+            rotated[(i+k)%n]= nums[i];
         }
-        int st=k;
-        int en=nums.size()-1;
-        while(st<en){
-            swap(nums[st++],nums[en--]);
-        }
-        int start=0;
-        int end=k-1;
-        while(start<end){
-            swap(nums[start++],nums[end--]);
+        for (int i=0;i<n;i++){
+            nums[i]=rotated[i];
         }
     }
 };
