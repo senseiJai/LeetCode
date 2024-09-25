05d8af7a9971aa890b20a8cfad9d67b13be66fc8
Jai@Khare <113430371+senseiJai@users.noreply.github.com>
2024-09-26 03:31:33 +0530

    Time: 0 ms (100%), Space: 7.5 MB (88.24%) - LeetHub

---
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        map<char,int> mpp;
+        for(int i=0;i<s.length();i++){
+            mpp[s[i]]++;
+            if(mpp[s[i]]==2){
+                return s[i];
+            }
+        }
+        return ' ';
+    }
+};
