d33a3fa01b23fe59782704bbb22b1c89b740577b
Jai@Khare <113430371+senseiJai@users.noreply.github.com>
2024-09-26 03:59:12 +0530

    Time: 10 ms (65.11%), Space: 14.5 MB (18.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,47 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for (int i=0;i<nums.size();i++){
-            for (int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
+        // vector<int> ans;
+        // for (int i=0;i<nums.size();i++){
+        //     for (int j=i+1;j<nums.size();j++){
+        //         if(nums[i]+nums[j]==target){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //         }
+        //     }
+        // }
+        // return ans;
+        // map <int , int> mp;
+        // vector<int> ans;
+        // for(int i=0;i<nums.size();i++){
+        //     mp[nums[i]]=i;
+        // }
+        // sort(nums.begin(),nums.end());
+        // int i=0;
+        // int j=nums.size()-1;
+        // while (i<j){
+        //     int sum=nums[i]+nums[j];
+        //     if(sum==target){
+        //         ans.push_back(mp[nums[i]]);
+        //         ans.push_back(mp[nums[j]]);
+        //         break;
+        //     }
+        //     if(sum>target){
+        //         j--;
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+        // return ans;
+        map<int , int> mp;
+        for(int i=0;i<nums.size();i++){
+            int rem=target-nums[i];
+            if(mp.find(rem)!=mp.end()){
+                return {mp[rem],i};
             }
+            mp[nums[i]]=i;
         }
-        return ans;
+        return {};
     }
 };
